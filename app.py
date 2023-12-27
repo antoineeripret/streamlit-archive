5b380a38a58bde86a3f99a64c0e115fac4f5a06e
antoineeripret <antoine.eripret@gmail.com>
2023-12-27 15:17:27 +0100

    fixed bugs

--- app.py
+++ app.py
@@ -39,21 +39,27 @@ if st.button('Extract data'):
 }
     headers = {'User-Agent':'download-application-streamlit'}
     r = requests.get('http://web.archive.org/cdx/search/cdx', params=params, headers=headers)
-    csv = pd.read_csv(StringIO(r.text), sep=' ')
-    csv.loc[len(csv)] = csv.columns.tolist()
-    csv.columns = ['urlkey','timestamp','url','type','rc','digest','length']
-    csv = csv[csv['url'].str.contains('|'.join(extensions_to_exclude), regex=True)==False]
-    csv['archive_url'] = 'http://web.archive.org/web/'+csv['timestamp'].astype(str)+'/'+csv['url']
-    csv['url'] = csv['url'].str.replace(':80','')
-    if parameters == False:
-        csv['url'] = csv['url'].str.replace('\?.*','' ,regex=True)
-    csv = csv[['url','type','archive_url']].drop_duplicates(subset='url')
-    csv = convert_df(csv)
-
-    st.download_button(
-        "Press to download file",
-        csv,
-        "file.csv",
-        "text/csv",
-        key='download-csv'
-        )
+    try: 
+      csv = pd.read_csv(StringIO(r.text), sep=' ')
+    except:
+      st.write('No data available for this domain.')
+      csv = None 
+    if csv is not None: 
+      csv.loc[len(csv)] = csv.columns.tolist()
+      csv.columns = ['urlkey','timestamp','url','type','rc','digest','length']
+      if extensions_to_exclude:
+        csv = csv[csv['url'].str.contains('|'.join(extensions_to_exclude), regex=True)==False]
+      csv['archive_url'] = 'http://web.archive.org/web/'+csv['timestamp'].astype(str)+'/'+csv['url']
+      csv['url'] = csv['url'].str.replace(':80','')
+      if parameters == False:
+          csv['url'] = csv['url'].str.replace('\?.*','' ,regex=True)
+      csv = csv[['url','type','archive_url']].drop_duplicates(subset='url')
+      csv = convert_df(csv)
+  
+      st.download_button(
+          "Press to download file",
+          csv,
+          "file.csv",
+          "text/csv",
+          key='download-csv'
+          )
